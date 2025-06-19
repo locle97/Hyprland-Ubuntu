8e54f72c3861d33d1432343a88fc3e22ddc4df50
Le Phat Loc <34123125+locle97@users.noreply.github.com>
2025-06-19 17:10:34 +0000

    feat: add installation of fish shell and set it as default for user

--- install_ricing.sh
+++ install_ricing.sh
@@ -2,6 +2,11 @@
 set -e
 echo "[+] Installing ricing tools and configs..."
 sudo npm install yarn -g
+echo "[+] Installing fish shell..."
+sudo apt update && sudo apt install -y fish
+echo "[+] Changing default shell to fish for user $USER..."
+chsh -s /usr/bin/fish
+
 echo "[+] Cloning tmux plugin manager..."
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm || true
 echo "[+] Cloning nvim config..."
@@ -12,6 +17,9 @@ cd ~/Downloads/
 wget https://github.com/jesseduffield/lazygit/releases/download/v0.52.0/lazygit_0.52.0_Linux_x86_64.tar.gz
 tar -xzf lazygit_0.52.0_Linux_x86_64.tar.gz
 rm ./lazygit_0.52.0_Linux_x86_64.tar.gz
+if [ ! -d ~/.local/bin ]; then
+  mkdir -p ~/.local/bin
+fi
 mv lazygit ~/.local/bin/
 cd -
 
