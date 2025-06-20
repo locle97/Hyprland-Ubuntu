92c2aa9b785a29788922a3dc7b49308ecbf0b2e7
Le Phat Loc <34123125+locle97@users.noreply.github.com>
2025-06-20 17:14:47 +0700

    Update install_hyprland.sh

--- install_hyprland.sh
+++ install_hyprland.sh
@@ -6,8 +6,21 @@ echo "[+] Installing dependencies..."
 # 0. Precondition: Install dependencies
 sudo apt update
 echo "[+] Installing apt packages..."
-sudo apt install -y meson wget build-essential ninja-build cmake-extras cmake gettext gettext-base fontconfig libfontconfig-dev libffi-dev libxml2-dev libdrm-dev libxkbcommon-x11-dev libxkbregistry-dev libxkbcommon-dev libpixman-1-dev libudev-dev libseat-dev seatd libxcb-dri3-dev libegl-dev libgles2 libegl1-mesa-dev glslang-tools libinput-bin libinput-dev libxcb-composite0-dev libavutil-dev libavcodec-dev libavformat-dev libxcb-ewmh2 libxcb-ewmh-dev libxcb-present-dev libxcb-icccm4-dev libxcb-render-util0-dev libxcb-res0-dev libxcb-xinput-dev libtomlplusplus3 libre2-dev
-
+sudo apt install -y \
+  meson wget build-essential ninja-build cmake-extras cmake gettext \
+  gettext-base fontconfig libfontconfig-dev libffi-dev libxml2-dev \
+  libdrm-dev libxkbcommon-x11-dev libxkbregistry-dev libxkbcommon-dev \
+  libpixman-1-dev libudev-dev libseat-dev seatd libxcb-dri3-dev \
+  libegl-dev libgles2 libegl1-mesa-dev glslang-tools libinput-bin \
+  libinput-dev libxcb-composite0-dev libavutil-dev libavcodec-dev \
+  libavformat-dev libxcb-ewmh2 libxcb-ewmh-dev libxcb-present-dev \
+  libxcb-icccm4-dev libxcb-render-util0-dev libxcb-res0-dev \
+  libxcb-xinput-dev libtomlplusplus3 libre2-dev \
+  libwayland* libgbm* libdisplay* libhyprlang* \
+  libhyprcursor* libcairo* libjpeg-dev libwebp-dev libmagic-dev \
+  libpango* libpangocairo* libxcursor* libxcb* libtomlplusplus-dev \
+  qt6* neofetch tmux eza zoxide fzf npm libpugixml-dev
+  
 echo "[+] Building hyprwayland-scanner v0.40 from source..."
 # 1. Build hyprwayland-scanner v0.40 from source
 git clone --depth 1 https://github.com/hyprwm/hyprwayland-scanner.git || true
